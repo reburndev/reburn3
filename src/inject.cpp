c6c33ce4d8e6648b43bb40ead9fd598c505393cb
itsmattkc <itsmattkc@gmail.com>
2023-04-17 16:56:14 +1000

    inject: temporarily disable graphics fix for experimentation

--- src/inject.cpp
+++ src/inject.cpp
@@ -26,17 +26,26 @@ void Inject()
   WriteMemory(0x2f845, &msg, sizeof(const wchar_t *));
   WriteMemory(0x2f87A, &msg, sizeof(const wchar_t *));*/
 
-  // Fix initial graphics and crashes
+  // Fix crashes
   WriteByte(0x3366D, 0x90);
   WriteBytes(0x33675, 0x90, 5);
   WriteBytes(0x44A0F, 0x90, 5);
-  WriteByte(0x1AE38A, 0x90);
+
+  // Fix initial graphics
+  /*WriteByte(0x1AE38A, 0x90);
   WriteBytes(0x1AE391, 0x90, 11);
-  WriteBytes(0x1AE731, 0x90, 11);
+  WriteBytes(0x1AE731, 0x90, 11);*/
+
+  // Force widescreen mode
+  //WriteMemory(0x3c8af, "\xb0\x01\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90", 17);
 
   // Inject 1920 display width
+  int w = 1920;
+  //WriteMemory(0x03d69c, &w, sizeof(w));
   //WriteMemory(0x1dd670, "\xba\x80\x07\x00\x00\x90", 6);
 
   // Inject 1080 display height
   //WriteMemory(0x1dd6b8, "\xba\x38\x04\x00\x00\x90", 6);
+
+  MessageBoxA(0, "Attach debugger now", 0, 0);
 }
